19600a1cd31ad0c2d9e82f4b115e953b33b888d0
Abhishek Chaudhary <abhishek97208@gmail.com>
2026-04-19 04:00:16 +0530

    added hardhat config files for solidity and networks

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@cofhe/hardhat-plugin";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    "eth-sepolia": {
+      url: process.env.BASE_SEPOLIA_RPC_URL || process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
